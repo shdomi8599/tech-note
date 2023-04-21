f9bca0adc3cc77bfffbe788c295d29f1f1c87f61
shdomi8599 <shdomi@naver.com>
2023-04-21 23:56:42 +0900

    refactor : react > next.js

---
+++ components/Header.tsx
@@ -0,0 +1,136 @@
+/* eslint-disable @next/next/no-img-element */
+import styled from "styled-components";
+import { CATEGORIES } from "@/constant/constant";
+import { FaBars } from "react-icons/fa";
+import { useMemo, useState } from "react";
+import Link from "next/link";
+import useOffResize from "@/hooks/useOffResize";
+
+const Header = () => {
+  const [togle, setTogle] = useState(false);
+  const togleHandler = () => {
+    setTogle(!togle);
+  };
+
+  const navData = useMemo(
+    () => (
+      <nav>
+        <ul>
+          {CATEGORIES.map((category) => (
+            <li key={category}>
+              <span>{category}</span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    ),
+    []
+  );
+
+  useOffResize(1040, "up", () => setTogle(false));
+
+  return (
+    <HeaderBox togle={togle}>
+      <div className="logo">
+        <Link href="/">
+          <img
+            src="https://www.cookapps.com/img/logo-white.76a0b6f4.svg"
+            alt="logo"
+          />
+        </Link>
+      </div>
+      <div className="main_nav">{navData}</div>
+      <div className="togle_btn">
+        <FaBars onClick={togleHandler} />
+      </div>
+      <div className="togle_nav">{navData}</div>
+    </HeaderBox>
+  );
+};
+
+export default Header;
+
+type HeaderBoxProps = {
+  togle: boolean;
+};
+
+const HeaderBox = styled.header<HeaderBoxProps>`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 12px calc((100% - 1400px) / 2);
+  align-items: center;
+  position: fixed;
+  background-color: var(--main-color);
+
+  .logo {
+    margin-left: 28px;
+  }
+
+  .main_nav {
+    margin-right: 28px;
+    color: white;
+    @media (max-width: 1040px) {
+      display: none;
+    }
+
+    ul {
+      display: flex;
+
+      li {
+        margin: 0px 20px;
+        font-size: 1.2rem;
+        font-weight: 600;
+        cursor: pointer;
+      }
+    }
+  }
+
+  .togle_btn {
+    display: none;
+    cursor: pointer;
+    margin-right: 28px;
+    color: white;
+    font-size: 1.5rem;
+    @media (max-width: 1040px) {
+      display: block;
+    }
+  }
+
+  .togle_nav {
+    position: absolute;
+    width: 100%;
+    top: 56px;
+
+    nav {
+      width: 100%;
+
+      ul {
+        display: none;
+        @media (max-width: 1040px) {
+          display: block;
+          width: 100%;
+          background-color: white;
+          color: #7a7878;
+          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+        }
+
+        li {
+          font-weight: 600;
+          font-size: 1.2rem;
+          padding: 14px 0px;
+          display: ${(props) => (props.togle ? "block" : "none")};
+          cursor: pointer;
+          :hover {
+            color: white;
+            background-color: black;
+          }
+
+          span {
+            margin-left: 28px;
+          }
+        }
+      }
+    }
+  }
+`;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,40 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
-export default function Document() {
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import type { DocumentContext, DocumentInitialProps } from "next/document";
+type Props = {
+  styles: React.ReactElement[];
+};
+const MyDocument = ({ styles }: Props) => {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+        {styles}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+
+MyDocument.getInitialProps = async (
+  ctx: DocumentContext
+): Promise<DocumentInitialProps & { styles: React.ReactElement[] }> => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = sheet.getStyleElement();
+    return { ...initialProps, styles };
+  } finally {
+    sheet.seal();
+  }
+};
+
+export default MyDocument;
