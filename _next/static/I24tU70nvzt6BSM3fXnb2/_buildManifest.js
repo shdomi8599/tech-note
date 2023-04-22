72e8641a2ca8edf2be4f8b10240ffeb5937af40d
shdomi8599 <shdomi@naver.com>
2023-04-23 03:02:37 +0900

    deploy to gh-pages

---
+++ _next/static/I24tU70nvzt6BSM3fXnb2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6cf0adff75968d80.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],"/[category]":["static/chunks/pages/[category]-bdf77e418b0ab93e.js"],sortedPages:["/","/_app","/_error","/[category]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
